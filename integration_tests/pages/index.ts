11b475e56162a751fe6ef36389147cb6530a5cfa
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-01-23 14:06:05 +0000

    Removing unused envs and fixing failing integration tests

--- integration_tests/pages/index.ts
+++ integration_tests/pages/index.ts
@@ -6,6 +6,4 @@ export default class IndexPage extends Page {
   }
 
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
-
-  headerPhaseBanner = (): PageElement => cy.get('[data-qa=header-phase-banner]')
 }
