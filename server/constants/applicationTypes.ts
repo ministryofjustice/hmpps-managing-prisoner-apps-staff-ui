2682d1e3215c356a4914a865c2295b651fa83358
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-01-24 09:08:29 +0000

    getting the application type page - WIP

--- server/constants/applicationTypes.ts
+++ server/constants/applicationTypes.ts
@@ -1,2 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
-export const APPLICATION_TYPES = [{ name: 'Swap visiting orders (VOs) for PIN credit' }]
+export const APPLICATION_TYPES = [
+  { name: 'Swap visiting orders (VOs) for PIN credit', value: 'swap-visiting-orders-for-pin-credit' },
+]
