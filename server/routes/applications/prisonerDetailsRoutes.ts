f916525230ce921344c6d1e9be7deefa9d72ef75
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-01-28 15:15:25 +0000

    create log prisoner details routes

---
+++ server/routes/applications/prisonerDetailsRoutes.ts
@@ -0,0 +1,28 @@
+import { Request, Response, Router } from 'express'
+import asyncMiddleware from '../../middleware/asyncMiddleware'
+import AuditService, { Page } from '../../services/auditService'
+
+export default function prisonerDetailsRoutes({ auditService }: { auditService: AuditService }): Router {
+  const router = Router()
+
+  router.get(
+    '/log/prisoner-details',
+    asyncMiddleware(async (req: Request, res: Response) => {
+      await auditService.logPageView(Page.LOG_PRISONER_DETAILS_PAGE, {
+        who: res.locals.user.username,
+        correlationId: req.id,
+      })
+
+      if (!req.session.data?.appType) {
+        return res.redirect('log/application-type')
+      }
+
+      return res.render('pages/log/prisoner-details', {
+        title: 'Log prisoner details',
+        appTypeTitle: req.session.data.appType.name,
+      })
+    }),
+  )
+
+  return router
+}
