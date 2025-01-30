3bc2ca19e174298e7980f0636e4f68b4cc36afa3
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-01-30 17:17:25 +0000

    Added route and place holder for swap vos details page

--- server/routes/applications/swapVosPinCreditDetailsRoutes.ts
+++ server/routes/applications/swapVosPinCreditDetailsRoutes.ts
@@ -2,13 +2,13 @@ import { Request, Response, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import AuditService, { Page } from '../../services/auditService'
 
-export default function prisonerDetailsRoutes({ auditService }: { auditService: AuditService }): Router {
+export default function swapVosPinCreditDetailsRoutes({ auditService }: { auditService: AuditService }): Router {
   const router = Router()
 
   router.get(
-    '/log/prisoner-details',
+    '/log/swap-vos-pin-credit-details',
     asyncMiddleware(async (req: Request, res: Response) => {
-      await auditService.logPageView(Page.LOG_PRISONER_DETAILS_PAGE, {
+      await auditService.logPageView(Page.LOG_SWAP_VOS_PIN_CREDIT_DETAILS_PAGE, {
         who: res.locals.user.username,
         correlationId: req.id,
       })
@@ -17,17 +17,10 @@ export default function prisonerDetailsRoutes({ auditService }: { auditService: 
         return res.redirect('log/application-type')
       }
 
-      return res.render('pages/log/prisoner-details', {
-        title: 'Log prisoner details',
+      return res.render('pages/log/swap-vos-pin-credit-details', {
+        title: 'Log swap VOs for PIN credit details',
         appTypeTitle: req.session.applicationData.type.name,
       })
-    }),
-  )
-
-  router.post(
-    '/log/prisoner-details',
-    asyncMiddleware(async (req: Request, res: Response) => {
-      res.redirect(`/log/swap-vos-pin-credit-details`)
     }),
   )
 
