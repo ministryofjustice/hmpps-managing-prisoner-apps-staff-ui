e167e27e51b054e7784dc7184d33cc1b735de6d2
Zak <57547218+madebyzak@users.noreply.github.com>
2025-02-04 11:08:52 +0000

    RA-209: create confirm swap vos pin credit details routes (#17)
    * create confirm swap vos pin credit details routes
    
    * add trailing newline

--- server/routes/applications/swapVosPinCreditDetailsRoutes.ts
+++ server/routes/applications/swapVosPinCreditDetailsRoutes.ts
@@ -27,5 +27,33 @@ export default function swapVosPinCreditDetailsRoutes({ auditService }: { auditS
     }),
   )
 
+  router.post(
+    '/log/swap-vos-pin-credit-details',
+    asyncMiddleware(async (req: Request, res: Response) => {
+      res.redirect(`/log/swap-vos-pin-credit-details/confirm`)
+    }),
+  )
+
+  router.get(
+    '/log/swap-vos-pin-credit-details/confirm',
+    asyncMiddleware(async (req: Request, res: Response) => {
+      await auditService.logPageView(Page.CONFIRM_SWAP_VOS_PIN_CREDIT_DETAILS_PAGE, {
+        who: res.locals.user.username,
+        correlationId: req.id,
+      })
+
+      const selectedAppType = APPLICATION_TYPES.find(type => type.value === req.session.applicationData?.type.value)
+
+      if (!selectedAppType) {
+        return res.redirect('/log/application-type')
+      }
+
+      return res.render('pages/log/confirm-swap-vos-pin-credit-details', {
+        title: 'Check details',
+        appTypeTitle: 'Swap VOs for PIN credit',
+      })
+    }),
+  )
+
   return router
 }
