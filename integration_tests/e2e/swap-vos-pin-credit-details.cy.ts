1ba25b69f9dd8266798f200a5a33c8f7b364f72e
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2025-02-07 11:32:08 +0000

    Feature/RA-247-swap vos pin credit details page tests (#19)
    * adding tests for swap vos pin credit details page
    
    * Fixing a linter check error
    
    * Trying the standard approach for testing
    
    * Fxing and trying the standard approach for testing
    
    * Fixing a test
    
    * Fix: improve test structure and fix errors in test execution
    
    * Refactoring the code
    
    * Renaming the file to make it consistent

---
+++ integration_tests/e2e/swap-vos-pin-credit-details.cy.ts
@@ -0,0 +1,49 @@
+import Page from '../pages/page'
+import SwapVosPinCreditDetailsPage from '../pages/swapVosPinCreditDetailsPage'
+
+context('Swap VOs for PIN Credit Details Page', () => {
+  let page: SwapVosPinCreditDetailsPage
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.signIn()
+    cy.visit('/log/swap-vos-pin-credit-details')
+
+    // Navigate through the pages
+    cy.contains('Swap visiting orders (VOs) for PIN credit').click()
+    cy.contains('button', 'Continue').click() // Click the Continue button on the 'Select application type' page
+    cy.contains('button', 'Continue').click() // Click the Continue button on the 'Log prisoner details' page
+    page = Page.verifyOnPage(SwapVosPinCreditDetailsPage)
+  })
+
+  it('should direct the user to the correct page', () => {
+    Page.verifyOnPage(SwapVosPinCreditDetailsPage)
+  })
+  it('should display the correct page title', () => {
+    page.pageTitle().should('include', 'Log swap VOs for PIN credit details')
+  })
+  it('should render the page heading correctly', () => {
+    page.checkOnPage()
+  })
+  it('should render the back link with correct text and href', () => {
+    page.backLink().should('have.text', 'Back').and('have.attr', 'href', '/log/prisoner-details')
+  })
+  it('should render the correct app type title', () => {
+    page.appTypeTitle().should('have.text', 'Swap VOs for PIN credit')
+  })
+  it('should render the correct form label for the textarea', () => {
+    page.formLabel().should('contain.text', 'Details (optional)')
+  })
+  it('should display the hint text correctly', () => {
+    page.hintText().should('contain.text', 'Add a brief summary, for example, if this person is a Foreign National')
+  })
+  it('should contain a textarea with the correct ID', () => {
+    page.textArea().should('have.attr', 'id', 'swap-vos-pin-credit-details')
+  })
+  it('should include a hidden CSRF token input field', () => {
+    page.csrfToken().should('exist')
+  })
+  it('should render a Continue button with the correct text', () => {
+    page.continueButton().should('contain.text', 'Continue')
+  })
+})
