52b528c328c21fb93ce7b47c2985430eeaf8e588
Zak <57547218+madebyzak@users.noreply.github.com>
2025-02-14 10:50:15 +0000

    RA-266: add managing apps api client + service (#30)
    * add managing apps api client + service
    
    * update docker values
    
    * refactor clients/services
    
    * update tests
    
    * update services exports
    
    * remove redundant mocks
    
    * update test data
    
    * fix test data
    
    * update config
    
    * update docker values
    
    * add user from res.locals
    
    * test config w/ e2e tests
    
    * fix typo

---
+++ server/@types/template.d.ts
@@ -0,0 +1,7 @@
+export type ActiveCaseLoad = {
+  caseLoadId: string
+  description: string
+  type: string
+  caseloadFunction: string
+  currentlyActive: boolean
+}
--- server/config.ts
+++ server/config.ts
@@ -93,6 +93,26 @@ export default {
       agent: new AgentConfig(Number(get('TOKEN_VERIFICATION_API_TIMEOUT_RESPONSE', 5000))),
       enabled: get('TOKEN_VERIFICATION_ENABLED', 'false') === 'true',
     },
+    managingPrisonerApps: {
+      url: get('MANAGING_PRISON_APPS_API_URL', 'http://localhost:8081', requiredInProduction),
+      healthPath: '/health/ping',
+      timeout: {
+        response: Number(get('MANAGING_PRISONER_APPS_API_TIMEOUT_RESPONSE', 10000)),
+        deadline: Number(get('MANAGING_PRISONER_APPS_API_TIMEOUT_RESPONSE', 10000)),
+      },
+      agent: new AgentConfig(Number(get('MANAGING_PRISONER_APPS_API_TIMEOUT_RESPONSE', 10000))),
+      enabled: get('MANAGING_PRISONER_APPS_ENABLED', 'false') === 'true',
+    },
+    hmppsManageUsers: {
+      url: get('HMPPS_MANAGE_USERS_API_URL', 'http://localhost:8080', requiredInProduction),
+      healthPath: '/health/ping',
+      timeout: {
+        response: Number(get('MANAGE_USERS_API_TIMEOUT_RESPONSE', 10000)),
+        deadline: Number(get('MANAGE_USERS_API_TIMEOUT_DEADLINE', 10000)),
+      },
+      agent: new AgentConfig(),
+      enabled: get('MANAGE_USERS_ENABLED', 'false') === 'true',
+    },
   },
   sqs: {
     audit: auditConfig(),
