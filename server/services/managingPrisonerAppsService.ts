7b97f43cc35d2fe045c9e36389b8d14d1ca8da0e
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-02-18 10:43:34 +0000

    add service tests

--- server/services/managingPrisonerAppsService.ts
+++ server/services/managingPrisonerAppsService.ts
@@ -1,11 +1,11 @@
 import { HmppsAuthClient } from '../data'
 import ManagingPrisonerAppsApiClient from '../data/managingPrisonerAppsClient'
-import { HmppsUser } from '../interfaces/hmppsUser'
+import { BaseUser } from '../interfaces/hmppsUser'
 
 export default class ManagingPrisonerAppsService {
   constructor(private readonly hmppsAuthClient: HmppsAuthClient) {}
 
-  async getPrisonerApp(prisonerId: string, applicationId: string, user: HmppsUser) {
+  async getPrisonerApp(prisonerId: string, applicationId: string, user: BaseUser) {
     const token = await this.hmppsAuthClient.getSystemClientToken(user.username)
     return new ManagingPrisonerAppsApiClient(token).getPrisonerApp(prisonerId, applicationId, user.token)
   }
