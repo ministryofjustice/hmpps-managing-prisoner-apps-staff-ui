013ccb4d1a18bd65408c286c0847f21d52048c41
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-03-17 13:17:07 +0000

    order statuses

--- server/constants/applicationStatus.ts
+++ server/constants/applicationStatus.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const APPLICATION_STATUS = {
-  PENDING: 'pending',
-  DECLINED: 'declined',
   APPROVED: 'approved',
+  DECLINED: 'declined',
+  PENDING: 'pending',
 } as const
