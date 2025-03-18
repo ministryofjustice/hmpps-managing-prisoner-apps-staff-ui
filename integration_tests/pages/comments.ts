ae870cf8ced4d44ac8e95585bf02fe349e21a8c9
Zak <57547218+madebyzak@users.noreply.github.com>
2025-03-18 14:18:40 +0000

    comments page setup (#51)

---
+++ integration_tests/pages/comments.ts
@@ -0,0 +1,19 @@
+import Page from './page'
+
+export default class CommentsPage extends Page {
+  constructor() {
+    super('Comments')
+  }
+
+  pageTitle = () => cy.title()
+
+  subNavigation = () => cy.get('.moj-sub-navigation')
+
+  activeTab = () => cy.get('.moj-sub-navigation__item a[aria-current="page"]')
+
+  commentBox = () => cy.get('#application-comment')
+
+  submitButton = () => cy.get('button.govuk-button--primary')
+
+  noCommentsMessage = () => cy.get('.app-messages-content p.none')
+}
