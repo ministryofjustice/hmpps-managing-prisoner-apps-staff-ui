fdae9a4beb2505a4d7f9a0a68111985d23f8cff2
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-04-02 14:56:32 +0100

    implement filters

--- server/routes/applications/viewApplicationsRoutes.ts
+++ server/routes/applications/viewApplicationsRoutes.ts
@@ -31,16 +31,36 @@ export default function viewApplicationRoutes({
       const statusQuery = req.query.status?.toString().toUpperCase()
       const status = statusQuery === 'CLOSED' ? ['APPROVED', 'DECLINED'] : ['PENDING']
 
+      const selectedDepartments: string[] = []
+      if (req.query.department) {
+        if (Array.isArray(req.query.department)) {
+          selectedDepartments.push(...req.query.department.map(dep => String(dep)))
+        } else {
+          selectedDepartments.push(String(req.query.department))
+        }
+      }
+
+      const selectedAppTypes: string[] = []
+      if (req.query.type) {
+        if (Array.isArray(req.query.type)) {
+          selectedAppTypes.push(...req.query.type.map(type => String(type)))
+        } else {
+          selectedAppTypes.push(String(req.query.type))
+        }
+      }
+
       const payload: ApplicationSearchPayload = {
         page: 1,
         size: 10,
         status,
-        types: [],
+        types: selectedAppTypes,
         requestedBy: null,
-        assignedGroups: [],
+        assignedGroups: selectedDepartments,
       }
 
-      const [{ apps, types }, prisonerDetails] = await Promise.all([
+      console.log({ payload })
+
+      const [{ apps, types, assignedGroups }, prisonerDetails] = await Promise.all([
         managingPrisonerAppsService.getApps(payload, user),
         managingPrisonerAppsService.getApps(payload, user).then(response =>
           Promise.all(
@@ -58,23 +78,77 @@ export default function viewApplicationRoutes({
         return { ...app, prisonerName }
       })
 
-      const groups = await managingPrisonerAppsService.getGroups(user)
-
       const appTypes = Object.entries(types)
         .map(([apiValue, count]) => {
           const matchingType = APPLICATION_TYPES.find(type => type.apiValue === apiValue)
-          return matchingType ? { value: matchingType.value, text: `${matchingType.name} (${count})` } : null
+          return matchingType
+            ? {
+                value: matchingType.apiValue,
+                text: `${matchingType.name} (${count})`,
+                checked: selectedAppTypes.includes(matchingType.apiValue),
+              }
+            : null
         })
         .filter(Boolean)
+
+      await auditService.logPageView(Page.VIEW_APPLICATIONS_PAGE, {
+        who: user.username,
+        correlationId: req.id,
+      })
+
+      const createDepartmentFilterHref = (departmentId: string) => {
+        const newQuery = new URLSearchParams(req.query as Record<string, string | string[]>)
+
+        newQuery.delete('department')
+
+        const departments = req.query.department
+        if (departments) {
+          const departmentArray = Array.isArray(departments) ? departments : [departments as string]
+          departmentArray
+            .filter(dep => dep !== departmentId)
+            .forEach(dep => newQuery.append('department', dep as string))
+        }
+
+        return `/applications?${newQuery.toString()}`
+      }
+
+      const createTypeFilterHref = (typeValue: string) => {
+        const newQuery = new URLSearchParams(req.query as Record<string, string | string[]>)
+
+        newQuery.delete('type')
+
+        const { types: qTypes } = req.query
+        if (qTypes) {
+          const typeArray = Array.isArray(qTypes) ? qTypes : [qTypes]
+          typeArray.filter(type => type !== typeValue).forEach(type => newQuery.append('type', type as string))
+        }
+
+        return `/applications?${newQuery.toString()}`
+      }
 
       res.render('pages/applications/list/index', {
         status: statusQuery || 'PENDING',
         apps: formatApplicationsToRows(appsWithNames),
-        departments: groups.map(group => ({
+        departments: assignedGroups.map(group => ({
           value: group.id,
-          text: group.name,
+          text: `${group.name} (${group.count})`,
+          checked: selectedDepartments.includes(group.id),
         })),
         appTypes,
+        selectedFilters: {
+          departments: assignedGroups
+            .filter(group => selectedDepartments.includes(group.id))
+            .map(group => ({
+              href: createDepartmentFilterHref(group.id),
+              text: group.name,
+            })),
+          appTypes: appTypes
+            .filter(type => type.checked)
+            .map(type => ({
+              href: createTypeFilterHref(type.value),
+              text: type.text,
+            })),
+        },
       })
     }),
   )
