19cf87112977b02a090db913c8d843755fc3cffd
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-04-02 17:53:59 +0100

    remove comments

--- server/utils/filters.ts
+++ server/utils/filters.ts
@@ -4,15 +4,13 @@ import { Request } from 'express'
 export const removeFilterFromHref = (req: Request, filterKey: string, valueToRemove: string) => {
   const newQuery = new URLSearchParams(req.query as Record<string, string | string[]>)
 
-  newQuery.delete(filterKey) // Remove all existing occurrences of the filter
+  newQuery.delete(filterKey)
 
   const queryValues = req.query[filterKey]
 
   if (queryValues) {
     const valuesArray = Array.isArray(queryValues) ? queryValues : [queryValues as string]
-    valuesArray
-      .filter(value => value !== valueToRemove) // Remove the selected value
-      .forEach(value => newQuery.append(filterKey, value as string)) // Re-add remaining values
+    valuesArray.filter(value => value !== valueToRemove).forEach(value => newQuery.append(filterKey, value as string))
   }
 
   return `/applications?${newQuery.toString()}`
