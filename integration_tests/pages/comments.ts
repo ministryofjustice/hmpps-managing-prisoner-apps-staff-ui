8887705cd1c4f8e2871e8adc4a181f51bb7b7eea
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-04-10 12:38:42 +0100

    fix id

--- integration_tests/pages/comments.ts
+++ integration_tests/pages/comments.ts
@@ -11,7 +11,7 @@ export default class CommentsPage extends Page {
 
   activeTab = () => cy.get('.moj-sub-navigation__item a[aria-current="page"]')
 
-  commentBox = () => cy.get('#application-comment')
+  commentBox = () => cy.get('#app-comment')
 
   submitButton = () => cy.get('button.govuk-button--primary')
 
