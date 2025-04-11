51142f74b26872cdec22a6be36ba5daf469ff7a2
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-04-11 10:13:47 +0100

    update e2e tests

--- integration_tests/mockApis/managingPrisonerApps.ts
+++ integration_tests/mockApis/managingPrisonerApps.ts
@@ -31,4 +31,18 @@ export default {
       },
     })
   },
+
+  stubGetAppResponse: ({ app }: { app: Application }): SuperAgentRequest => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        url: `/v1/prisoners/${app.requestedBy.username}/apps/${app.id}/responses/${app.requests[0].responseId}?createdBy=true`,
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: new TestData().response,
+      },
+    })
+  },
 }
