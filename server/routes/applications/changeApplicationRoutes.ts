5709dd35580388e15405e81b5f405e619f6e3bb5
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-04-29 09:54:37 +0100

    Retaining form values on back button and tidy up - check details page

--- server/routes/applications/changeApplicationRoutes.ts
+++ server/routes/applications/changeApplicationRoutes.ts
@@ -38,6 +38,11 @@ export default function changeApplicationRoutes({
         return res.redirect(`/applications?error=unknown-type`)
       }
 
+      const { applicationData } = req.session
+      if (applicationData?.additionalData) {
+        Object.assign(application, applicationData.additionalData)
+      }
+
       return res.render(`pages/applications/change/index`, {
         application,
         applicationType,
