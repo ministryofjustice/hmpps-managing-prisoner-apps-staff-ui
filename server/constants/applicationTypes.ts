f3aa25c0b7af4d30a858538642323791e057ccf7
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2025-05-08 11:14:53 +0100

    RA-362: Adding supply list pin contacts app type (#100)
    * Adding supply list pin contacts app
    
    * Tests for each app type
    
    * reusing the existing code avoid duplicates

--- server/constants/applicationTypes.ts
+++ server/constants/applicationTypes.ts
@@ -1,5 +1,6 @@
 export enum APPLICATION_TYPE_VALUES {
   PIN_PHONE_EMERGENCY_CREDIT_TOP_UP = 'add-emergency-pin-phone-credit',
+  PIN_PHONE_SUPPLY_LIST_OF_CONTACTS = 'supply-list-of-pin-phone-contacts',
   PIN_PHONE_CREDIT_SWAP_VISITING_ORDERS = 'swap-visiting-orders-for-pin-credit',
 }
 
@@ -8,6 +9,11 @@ export const APPLICATION_TYPES = [
     name: 'Add emergency PIN phone credit',
     value: APPLICATION_TYPE_VALUES.PIN_PHONE_EMERGENCY_CREDIT_TOP_UP,
     apiValue: 'PIN_PHONE_EMERGENCY_CREDIT_TOP_UP',
+  },
+  {
+    name: 'Supply list of PIN phone contacts',
+    value: APPLICATION_TYPE_VALUES.PIN_PHONE_SUPPLY_LIST_OF_CONTACTS,
+    apiValue: 'PIN_PHONE_SUPPLY_LIST_OF_CONTACTS',
   },
   {
     name: 'Swap visiting orders (VOs) for PIN credit',
