57ff46d5d2f7c5ab5fd1518e513b7397da0b713e
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-05-21 15:08:17 +0100

    update mock

--- integration_tests/mockApis/prison.ts
+++ integration_tests/mockApis/prison.ts
@@ -1,28 +1,13 @@
 import { SuperAgentRequest } from 'superagent'
-import jwt from 'jsonwebtoken'
-import { stubFor } from './wiremock'
 import TestData from '../../server/routes/testutils/testData'
-
-const generateMockToken = () => {
-  const payload = {
-    user_name: 'user1',
-    scope: ['read', 'write'],
-    auth_source: 'nomis',
-  }
-
-  return jwt.sign(payload, 'secret', { expiresIn: '1h' })
-}
+import { stubFor } from './wiremock'
 
 export default {
   stubGetPrisonerByPrisonNumber: (prisonNumber = 'G3682UE'): SuperAgentRequest => {
-    const token = generateMockToken()
     return stubFor({
       request: {
         method: 'GET',
         url: `/prison/api/prisoners/${prisonNumber}`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
       },
       response: {
         status: 200,
