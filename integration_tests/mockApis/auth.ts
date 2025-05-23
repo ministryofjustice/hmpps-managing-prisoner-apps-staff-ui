13befd3a758c754fa838d3d57f941a2482e82ebc
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2025-05-23 14:17:13 +0100

    RA-459: Block unauthed users (#122)
    * RA-459: Block unauthed users
    
    * Fix int tests signin
    
    * Fix int tests signin

--- integration_tests/mockApis/auth.ts
+++ integration_tests/mockApis/auth.ts
@@ -130,6 +130,8 @@ export default {
   getSignInUrl,
   stubAuthPing: ping,
   stubAuthManageDetails: manageDetails,
-  stubSignIn: (userToken: UserToken = {}): Promise<[Response, Response, Response, Response, Response]> =>
+  stubSignIn: (
+    userToken = { roles: ['ROLE_MANAGING_PRISONER_APPS'] },
+  ): Promise<[Response, Response, Response, Response, Response]> =>
     Promise.all([favicon(), redirect(), signOut(), token(userToken), tokenVerification.stubVerifyToken()]),
 }
