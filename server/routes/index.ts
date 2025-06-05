2e4a41084ab38fd325c0dcc7089ad21804d843eb
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-06-05 10:49:26 +0100

    Adding accessibility statement page

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import type { Services } from '../services'
 
 import applicationsRoutes from './applications'
+import staticPagesRoutes from './static-pages/staticPagesRoutes'
 
 export default function routes({ auditService, managingPrisonerAppsService, prisonService }: Services): Router {
   const router = Router()
 
   router.use('/', applicationsRoutes({ auditService, managingPrisonerAppsService, prisonService }))
+  router.use('/', staticPagesRoutes())
 
   return router
 }
