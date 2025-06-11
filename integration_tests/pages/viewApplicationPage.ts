83d79349bfa9cf02b3c5565892dd3e816074c189
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-06-11 10:12:20 +0100

    remove application id from view app

--- integration_tests/pages/viewApplicationPage.ts
+++ integration_tests/pages/viewApplicationPage.ts
@@ -19,8 +19,6 @@ export default class ViewApplicationPage extends Page {
 
   submittedOn = () => cy.get('.govuk-summary-list__row').contains('Date sent').next()
 
-  applicationId = () => cy.get('.govuk-summary-list__row').contains('Application ID').next()
-
   forwardApplication = () => cy.get('.govuk-summary-list__row').contains('Department').parent().find('a')
 
   commentsTab = () => cy.contains('.moj-sub-navigation__link', 'Comments')
