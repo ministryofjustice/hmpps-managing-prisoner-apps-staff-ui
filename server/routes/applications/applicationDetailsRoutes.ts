05354f659ea820479322c9bf7df8e9428441339d
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-08-27 21:17:55 +0100

    persist data

--- server/routes/applications/applicationDetailsRoutes.ts
+++ server/routes/applications/applicationDetailsRoutes.ts
@@ -58,6 +58,7 @@ export default function applicationDetailsRoutes({
         title: 'Log details',
         ...templateFields,
         applicationType,
+        earlyDaysCentre: applicationData?.earlyDaysCentre,
       })
     }),
   )
