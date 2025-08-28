ed4b7211904c1c375816dfc17d594ff166cf51a8
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-08-28 21:11:50 +0100

    update e2e tests

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -1,9 +1,33 @@
 declare namespace Cypress {
   interface Chainable {
     /**
-     * Custom command to signIn. Set failOnStatusCode to false if you expect and non 200 return code
-     * @example cy.signIn({ failOnStatusCode: boolean })
+     * Custom command to sign in. Set failOnStatusCode to false if you expect a non-200 return code.
+     * @example cy.signIn({ failOnStatusCode: false })
      */
     signIn(options?: { failOnStatusCode: boolean }): Chainable<AUTWindow>
+
+    /**
+     * Resets the test state and signs in.
+     * @example cy.resetAndSignIn()
+     */
+    resetAndSignIn(): Chainable<void>
+
+    /**
+     * Visits the index page and starts a new application.
+     * @example cy.visitIndexAndStartApplication()
+     */
+    visitIndexAndStartApplication(): Chainable<void>
+
+    /**
+     * Enters prisoner details and continues.
+     * @example cy.enterPrisonerDetails()
+     */
+    enterPrisonerDetails(): Chainable<void>
+
+    /**
+     * Selects an application type and continues.
+     * @example cy.selectApplicationType('Add new social PIN phone contact')
+     */
+    selectApplicationType(appType: string): Chainable<void>
   }
 }
--- integration_tests/support/commands.ts
+++ integration_tests/support/commands.ts
@@ -2,3 +2,27 @@ Cypress.Commands.add('signIn', (options = { failOnStatusCode: true }) => {
   cy.request('/')
   return cy.task('getSignInUrl').then((url: string) => cy.visit(url, options))
 })
+
+Cypress.Commands.add('resetAndSignIn', () => {
+  cy.task('reset')
+  cy.task('stubSignIn')
+  cy.signIn()
+})
+
+Cypress.Commands.add('visitIndexAndStartApplication', () => {
+  cy.visit('/')
+  cy.contains('a', 'Log a new application').click()
+  cy.task('stubGetAppTypes')
+})
+
+Cypress.Commands.add('enterPrisonerDetails', () => {
+  cy.task('stubGetPrisonerByPrisonerNumber', 'A1234AA')
+  cy.get('#prison-number').type('A1234AA')
+  cy.contains('button', 'Find prisoner').click()
+  cy.contains('button', 'Continue').click()
+})
+
+Cypress.Commands.add('selectApplicationType', (appType: string) => {
+  cy.contains(appType).click()
+  cy.contains('button', 'Continue').click()
+})
