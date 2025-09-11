e00ca688ba21be772a47f6d3fae14c87f9de6993
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-09-11 14:04:36 +0100

    Code refactoring

--- server/routes/validate/validateNewLegalContact.ts
+++ server/routes/validate/validateNewLegalContact.ts
@@ -27,27 +27,25 @@ export const validateAddNewLegalContact = (form: AddNewLegalPinPhoneContactDetai
     errors.relationship = { text: 'Select a relationship' }
   }
 
-  const telephone1 = form.telephone1?.trim() || ''
-  if (!telephone1) {
-    errors.telephone1 = { text: errorMessages.phoneRequired }
-  } else {
-    const result = validatePhoneNumber(telephone1)
-    if (result === 'invalid_format') {
-      errors.telephone1 = { text: errorMessages.invalidFormat }
-    } else if (result === 'invalid_number') {
-      errors.telephone1 = { text: errorMessages.invalidNumber }
+  function validateAndAssignError(fieldName: 'telephone1' | 'telephone2', isRequired: boolean) {
+    const value = form[fieldName]?.trim() || ''
+    if (isRequired && !value) {
+      errors[fieldName] = { text: errorMessages.phoneRequired }
+      return
+    }
+
+    if (value) {
+      const result = validatePhoneNumber(value)
+      if (result === 'invalid_format') {
+        errors[fieldName] = { text: errorMessages.invalidFormat }
+      } else if (result === 'invalid_number') {
+        errors[fieldName] = { text: errorMessages.invalidNumber }
+      }
     }
   }
 
-  const telephone2 = form.telephone2?.trim() || ''
-  if (telephone2) {
-    const result = validatePhoneNumber(telephone2)
-    if (result === 'invalid_format') {
-      errors.telephone2 = { text: errorMessages.invalidFormat }
-    } else if (result === 'invalid_number') {
-      errors.telephone2 = { text: errorMessages.invalidNumber }
-    }
-  }
+  validateAndAssignError('telephone1', true)
+  validateAndAssignError('telephone2', false)
 
   return errors
 }
