22a9596fd64f1f08393b40e7971f8046f069bafb
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-10-29 13:25:48 +0000

    remove console logs

--- server/routes/applications/appType.ts
+++ server/routes/applications/appType.ts
@@ -41,9 +41,7 @@ export default function appTypeRouter({
       }
 
       const groups = await managingPrisonerAppsService.getGroupsAndTypes(user)
-      console.log('Groups:', groups)
       const selectedGroup = groups.find(group => group.id.toString() === applicationData.group.value)
-      console.log('Selected Group:', selectedGroup)
 
       if (!selectedGroup) {
         return res.redirect(URLS.LOG_GROUP)
