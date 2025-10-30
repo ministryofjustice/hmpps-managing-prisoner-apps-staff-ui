0af35d151f330140c3ad987e71e566ef1eab690c
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2025-10-30 10:33:28 +0000

    fix app type labels

--- server/constants/applicationTypes.ts
+++ server/constants/applicationTypes.ts
@@ -8,10 +8,10 @@ export enum APPLICATION_TYPE_VALUES {
 }
 
 export const applicationTypeLabels: Record<keyof typeof APPLICATION_TYPE_VALUES, string> = {
-  PIN_PHONE_ADD_NEW_SOCIAL_CONTACT: 'Add new social PIN phone contact',
-  PIN_PHONE_ADD_NEW_OFFICIAL_CONTACT: 'Add new official PIN phone contact',
-  PIN_PHONE_EMERGENCY_CREDIT_TOP_UP: 'Add emergency PIN phone credit',
-  PIN_PHONE_CREDIT_SWAP_VISITING_ORDERS: 'Swap visiting orders (VOs) for PIN credit',
+  PIN_PHONE_ADD_NEW_SOCIAL_CONTACT: 'Add a social PIN phone contact',
+  PIN_PHONE_ADD_NEW_OFFICIAL_CONTACT: 'Add an official PIN phone contact',
+  PIN_PHONE_EMERGENCY_CREDIT_TOP_UP: 'Add emergency phone credit',
+  PIN_PHONE_CREDIT_SWAP_VISITING_ORDERS: 'Swap Visiting Orders (VOs) for PIN Credit',
   PIN_PHONE_SUPPLY_LIST_OF_CONTACTS: 'Supply list of PIN phone contacts',
-  PIN_PHONE_REMOVE_CONTACT: 'Remove PIN phone contact',
+  PIN_PHONE_REMOVE_CONTACT: 'Add or remove a PIN phone contact',
 }
