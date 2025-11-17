b3401c613009d83733e852bfb1995c418ccc23b5
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2025-11-17 10:03:53 +0000

    fixing application list display (#269)

--- server/helpers/filters/formatAppTypesForFilters.ts
+++ server/helpers/filters/formatAppTypesForFilters.ts
@@ -1,29 +1,13 @@
-import { HmppsUser } from '../../interfaces/hmppsUser'
-import ManagingPrisonerAppsService from '../../services/managingPrisonerAppsService'
-import { appTypeIdToLegacyKeyMap } from '../../testData/appTypes'
-
 // eslint-disable-next-line import/prefer-default-export
-export const formatAppTypesForFilters = async (
-  managingPrisonerAppsService: ManagingPrisonerAppsService,
-  user: HmppsUser,
+export const formatAppTypesForFilters = (
   applicationTypes: Record<string, { id: number; name: string; count: number }>,
   selectedFilters: { types: string[] },
 ) => {
-  const appTypes = await managingPrisonerAppsService.getAppTypes(user)
   return Object.entries(applicationTypes)
-    .filter(([, value]) => value.count > 0)
-    .map(([_, value]) => {
-      const serviceKey = appTypeIdToLegacyKeyMap[value.id]
-      if (!serviceKey) return null
-
-      const matchingType = appTypes.find(type => type.key === serviceKey)
-      if (!matchingType) return null
-
-      return {
-        value: value.id.toString(),
-        text: `${matchingType.name} (${value.count})`,
-        checked: selectedFilters.types.includes(value.id.toString()),
-      }
-    })
-    .filter(Boolean)
+    .filter(([_, value]) => value.count > 0)
+    .map(([_, value]) => ({
+      value: value.id.toString(),
+      text: `${value.name} (${value.count})`,
+      checked: selectedFilters.types.includes(value.id.toString()),
+    }))
 }
