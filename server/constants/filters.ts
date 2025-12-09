996ada4789a973ef3203bcd52da4e7ea6eadb144
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2025-12-09 10:10:38 +0000

    RA-743: retain filters in session until cleared (#292)
    * retain filters in session until cleared
    
    * Adding import constants

---
+++ server/constants/filters.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+export const FILTER_KEYS = [
+  'order',
+  'status',
+  'prisoner',
+  'priority',
+  'group',
+  'applicationTypeFilter',
+  'type',
+] as const
