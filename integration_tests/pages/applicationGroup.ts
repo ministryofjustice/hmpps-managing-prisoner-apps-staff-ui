c7774d646e3991ac7a185ad6cfade881046ce48f
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-12-09 14:55:57 +0000

    application group logging journey page e2e test

--- integration_tests/pages/applicationGroup.ts
+++ integration_tests/pages/applicationGroup.ts
@@ -8,4 +8,14 @@ export default class ApplicationGroupPage extends Page {
   pageTitle = () => cy.title()
 
   backLink = () => cy.get('.govuk-back-link')
+
+  radioButton = () => cy.get('input[type="radio"][name="group"]')
+
+  applicationGroupLabel = () => cy.contains('label', 'Pin Phone Contact Apps')
+
+  submitButton = () => cy.get('button[type="submit"]')
+
+  errorSummary = () => cy.get('.govuk-error-summary')
+
+  errorMessage = () => cy.get('.govuk-error-message')
 }
