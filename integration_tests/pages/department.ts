c661ea71b9631fcd13c76c4804395493168b2666
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2025-12-09 15:23:34 +0000

    department page logging journey e2e test

--- integration_tests/pages/department.ts
+++ integration_tests/pages/department.ts
@@ -8,4 +8,14 @@ export default class DepartmentPage extends Page {
   pageTitle = () => cy.title()
 
   backLink = () => cy.get('.govuk-back-link')
+
+  radioButton = () => cy.get('.govuk-radios__item')
+
+  continueButton = () => cy.get('.govuk-button')
+
+  errorSummary = () => cy.get('.govuk-error-summary')
+
+  errorMessage = () => cy.get('.govuk-error-message')
+
+  departmentLabel = () => cy.contains('label', 'Business Hub')
 }
