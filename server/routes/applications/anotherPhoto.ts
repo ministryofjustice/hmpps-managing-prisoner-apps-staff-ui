9518b56ecaf5a8569c7b25946a0d967f817fee62
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2026-02-09 09:31:55 +0000

    add another photo page (#330)

--- server/routes/applications/anotherPhoto.ts
+++ server/routes/applications/anotherPhoto.ts
@@ -6,6 +6,10 @@ import { URLS } from '../../constants/urls'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 import AuditService, { Page } from '../../services/auditService'
+import { updateSessionData } from '../../utils/http/session'
+
+const ERROR_MESSAGE = 'Select one'
+const ERROR_SUMMARY = 'You need to select if you want to take another photo of the application.'
 
 export default function anotherPhotoRouter({ auditService }: { auditService: AuditService }): Router {
   const router = Router()
@@ -20,6 +24,12 @@ export default function anotherPhotoRouter({ auditService }: { auditService: Aud
         return res.redirect(URLS.LOG_METHOD)
       }
 
+      const photos = applicationData.photos || []
+
+      if (photos.length >= 2) {
+        return res.redirect(URLS.LOG_ADDITIONAL_PHOTO_DETAILS)
+      }
+
       await auditService.logPageView(Page.LOG_ADD_ANOTHER_PHOTO_PAGE, {
         who: user.username,
         correlationId: req.id,
@@ -30,7 +40,41 @@ export default function anotherPhotoRouter({ auditService }: { auditService: Aud
         applicationType: applicationData.type.name,
         LOG_ADD_ANOTHER_PHOTO: URLS.LOG_ADD_ANOTHER_PHOTO,
         backLink: URLS.LOG_CONFIRM_PHOTO_CAPTURE,
+        addAnotherPhoto: applicationData.addAnotherPhoto || null,
       })
+    }),
+  )
+
+  router.post(
+    URLS.LOG_ADD_ANOTHER_PHOTO,
+    asyncMiddleware(async (req: Request, res: Response) => {
+      const { applicationData } = req.session
+      const { addAnotherPhoto } = req.body
+
+      if (!applicationData?.loggingMethod) {
+        return res.redirect(URLS.LOG_METHOD)
+      }
+
+      if (!addAnotherPhoto) {
+        return res.render(PATHS.LOG_APPLICATION.ADD_ANOTHER_PHOTO, {
+          title: 'Do you want to add another photo?',
+          applicationType: applicationData.type?.name ?? '',
+          errorMessage: ERROR_MESSAGE,
+          errorSummary: [{ text: ERROR_SUMMARY, href: '#addAnotherPhoto' }],
+          addAnotherPhoto: addAnotherPhoto || null,
+          backLink: URLS.LOG_CONFIRM_PHOTO_CAPTURE,
+        })
+      }
+
+      updateSessionData(req, { addAnotherPhoto })
+
+      if (addAnotherPhoto === 'yes') {
+        return res.redirect(URLS.LOG_PHOTO_CAPTURE)
+      }
+      if (addAnotherPhoto === 'no') {
+        return res.redirect(URLS.LOG_ADDITIONAL_PHOTO_DETAILS)
+      }
+      return res.redirect(URLS.LOG_METHOD)
     }),
   )
 
