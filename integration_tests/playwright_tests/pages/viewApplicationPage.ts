0476f897c54575dd7786bd5836a7188372f1912a
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2026-04-02 09:12:03 +0100

    Add Playwright tests for viewing application pages with and without photos (#355)

---
+++ integration_tests/playwright_tests/pages/viewApplicationPage.ts
@@ -0,0 +1,90 @@
+import { type Page as PlaywrightPage } from '@playwright/test'
+import Page, { type PageElement } from './page'
+
+export default class ViewApplicationPage extends Page {
+  constructor(page: PlaywrightPage) {
+    // Extract the application type name from the h1
+    super(page, '')
+  }
+
+  async checkOnPage(): Promise<void> {
+    // Verify we're on a view application page by checking for the expected sections
+    await this.page.locator('.moj-sub-navigation').waitFor()
+  }
+
+  pageTitle(): Promise<string> {
+    return this.page.title()
+  }
+
+  applicationType(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Application type") .govuk-summary-list__value')
+  }
+
+  applicationStatus(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Status") .govuk-summary-list__value')
+  }
+
+  department(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Department") .govuk-summary-list__value')
+  }
+
+  prisonerName(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Prisoner") .govuk-summary-list__value')
+  }
+
+  prisonerCellLocation(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Location") .govuk-summary-list__value')
+  }
+
+  submittedOn(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has(dt:text-is("Date")) .govuk-summary-list__value')
+  }
+
+  forwardApplication(): PageElement {
+    return this.page.locator('.govuk-summary-list__row:has-text("Department") a')
+  }
+
+  commentsTab(): PageElement {
+    return this.page.locator('.moj-sub-navigation__link:has-text("Comments")')
+  }
+
+  actionAndReplyTab(): PageElement {
+    return this.page.locator('.moj-sub-navigation__link:has-text("Action and reply")')
+  }
+
+  historyTab(): PageElement {
+    return this.page.locator('.moj-sub-navigation__link:has-text("History")')
+  }
+
+  viewProfileLink(): PageElement {
+    return this.page.locator('a:has-text("View profile")')
+  }
+
+  viewAlertsLink(): PageElement {
+    return this.page.locator('a:has-text("View alerts")')
+  }
+
+  summaryListRowKey(keyText: string): PageElement {
+    return this.page.locator('.govuk-summary-list__key', { hasText: keyText })
+  }
+
+  image1Label(): PageElement {
+    return this.page.locator('dt:has-text("Image 1")')
+  }
+
+  image2Label(): PageElement {
+    return this.page.locator('dt:has-text("Image 2 (optional)")')
+  }
+
+  thumbnailImages(): PageElement {
+    return this.page.locator('img.thumbnail-img')
+  }
+
+  additionalDetailsLabel(): PageElement {
+    return this.page.locator('dt:has-text("Additional details")')
+  }
+
+  changeButton(): PageElement {
+    return this.page.locator('.govuk-summary-list--header:has-text("Change")')
+  }
+}
