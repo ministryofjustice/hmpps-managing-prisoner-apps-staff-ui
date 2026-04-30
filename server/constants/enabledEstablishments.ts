e7fc2db82eaf8c3cf909c62b11c437d1cd45e299
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2026-04-30 10:26:30 +0100

    enabling log method page for low newton establishment (#364)

--- server/constants/enabledEstablishments.ts
+++ server/constants/enabledEstablishments.ts
@@ -1,4 +1,4 @@
-export const LOG_METHOD_ENABLED_ESTABLISHMENTS: ReadonlyArray<string> = ['HMI', 'LEI']
+export const LOG_METHOD_ENABLED_ESTABLISHMENTS: ReadonlyArray<string> = ['HMI', 'LEI', 'LNI']
 
 export const isLogMethodEnabledEstablishment = (activeCaseLoadId?: string): boolean =>
   Boolean(activeCaseLoadId && LOG_METHOD_ENABLED_ESTABLISHMENTS.includes(activeCaseLoadId))
