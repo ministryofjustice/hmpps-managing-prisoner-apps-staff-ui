c51408ac4639544e8e38017f96d6d357a55bba4a
Sweety Mariam John <162119856+sweetymj@users.noreply.github.com>
2026-09-08 11:13:46 +0100

    Document camera feature for wealstun establishment (#433)

--- server/constants/enabledEstablishments.ts
+++ server/constants/enabledEstablishments.ts
@@ -1,4 +1,12 @@
-export const LOG_METHOD_ENABLED_ESTABLISHMENTS: ReadonlyArray<string> = ['HMI', 'LEI', 'LNI', 'MDI', 'HHI', 'HLI']
+export const LOG_METHOD_ENABLED_ESTABLISHMENTS: ReadonlyArray<string> = [
+  'HMI',
+  'LEI',
+  'LNI',
+  'MDI',
+  'HHI',
+  'HLI',
+  'WEI',
+]
 
 export const isLogMethodEnabledEstablishment = (activeCaseLoadId?: string): boolean =>
   Boolean(activeCaseLoadId && LOG_METHOD_ENABLED_ESTABLISHMENTS.includes(activeCaseLoadId))
