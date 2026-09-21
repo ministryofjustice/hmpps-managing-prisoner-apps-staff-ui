b009c2eb90f3405096250319bce9fd29387ba35c
Sweety Mariam John <sweety.john@digital.justice.gov.uk>
2026-09-21 12:24:41 +0100

    add status column

--- server/utils/formatters/formatAppsToRows.ts
+++ server/utils/formatters/formatAppsToRows.ts
@@ -1,6 +1,7 @@
 import { format, getTime } from 'date-fns'
 
 import { ViewAppListApp } from '../../@types/managingAppsApi'
+import { getStatusTag } from '../../constants/applicationStatus'
 import { getAppType } from '../../helpers/application/getAppType'
 import { HmppsUser } from '../../interfaces/hmppsUser'
 import ManagingPrisonerAppsService from '../../services/managingPrisonerAppsService'
@@ -21,13 +22,14 @@ export const formatAppsToRows = async (
 ) => {
   return Promise.all(
     applications.map(async application => {
-      const { createdDate, appType, requestedBy, assignedGroup, id, prisonerName, comments } = application
+      const { createdDate, appType, requestedBy, assignedGroup, id, prisonerName, comments, status } = application
 
       const date = new Date(createdDate)
       const formattedDate = format(date, 'dd/MM/yyyy')
       const sortValue = getTime(date).toString()
 
       const type = await getAppType(managingPrisonerAppsService, user, appType.id.toString())
+      const statusTag = getStatusTag(status)
 
       const row = [
         { text: formattedDate, attributes: { 'data-sort-value': sortValue }, classes: 'govuk-!-text-nowrap' },
@@ -35,7 +37,10 @@ export const formatAppsToRows = async (
           html: `${type?.name}<br/><span class="govuk-table__subtext govuk-body-s">${assignedGroup?.name || 'N/A'}</span>`,
         },
         {
-          html: `${prisonerName}<br/><span class="govuk-table__subtext govuk-body-s">${requestedBy}</span>`,
+          html: `<span class="govuk-!-text-nowrap">${prisonerName}</span><br/><span class="govuk-table__subtext govuk-body-s">${requestedBy}</span>`,
+        },
+        {
+          html: `<strong class="govuk-tag ${statusTag.classes} govuk-!-text-nowrap">${statusTag.text}</strong>`,
         },
         {
           html: comments > 0 ? mojNotificationBadge({ text: `${comments}` }) : 'None',
